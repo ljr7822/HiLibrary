d377a836ec92cd938ff5f4629125778668b893f5
iwenLjr <1816807688@qq.com>
2021-01-25 01:40:18 +0800

    初始化工程

---
+++ hilibrary/src/main/java/com/example/iwen/hilibrary/log/HiLog.java
@@ -0,0 +1,17 @@
+package com.example.iwen.hilibrary.log;
+
+import java.util.Objects;
+
+/**
+ * 1.打印堆栈信息
+ * 2.File输出
+ * 3.模拟控制台
+ *
+ * @author : iwen大大怪
+ * create : 2021/01/25 1:35
+ */
+public class HiLog {
+    public static void v(Object... contents){
+
+    }
+}
---
+++ hilibrary/src/main/java/com/example/iwen/hilibrary/log/HiLogType.java
@@ -0,0 +1,18 @@
+package com.example.iwen.hilibrary.log;
+
+import android.util.Log;
+
+/**
+ * 定义日志的类型
+ *
+ * @author : iwen大大怪
+ * create : 2021/01/25 1:37
+ */
+public class HiLogType {
+    public final static int V = Log.VERBOSE;
+    public final static int D = Log.DEBUG;
+    public final static int I = Log.INFO;
+    public final static int W = Log.WARN;
+    public final static int E = Log.ERROR;
+    public final static int A = Log.ASSERT;
+}
